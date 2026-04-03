260379105448ba1b422f3766c727a514faddf85a
Priyanka Asthana <166942678+PriyankaAsthana@users.noreply.github.com>
2026-04-03 17:47:40 +0530

    Update ExperienceTopLeft.jsx

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -6,9 +6,9 @@ const ExperienceTopLeft = () => {
     <div className='flex flex-col gap-6 w-[300px]'>
       <p className='text-orange font-bold uppercase text-3xl font-special text-center'>Since 2024</p>
       <div className='flex justify-center items-center gap-4'>
-        <ExperienceInfo number='1' text='Years'/>
+        <ExperienceInfo number='2' text='Years'/>
         <p className='font-bold text-6xl text-lightbrown'>-</p>
-        <ExperienceInfo number='5' text='Websites'/>
+        <ExperienceInfo number='6' text='Websites'/>
       </div>
       <p className='text-center text-white'>Crafting clean code and real-world<br/>
         solutions at the intersection of Java,<br/>
